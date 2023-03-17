38c92d153f9f8a2413a844b019a087678f085175
yur.raz@mail.ru <yur.raz@mail.ru>
2023-03-17 16:50:09 +0300

    Virtual environment and liabraries

---
+++ Requiments.bat
@@ -0,0 +1,9 @@
+@rem git clone https://github.com/actggg/9-class-olimp.git
+cd venv\Scripts
+pip install --upgrade pip
+pip3 install pyqt5
+pip install nltk
+pip install requests
+pip install bs4
+pip install scikit-learn
+% вроде не нужен pip install pickle
